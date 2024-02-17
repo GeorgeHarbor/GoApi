18c349b61f75ce7efc194189ef9ad146e94b4710
GeorgeHarbor <119446416+GeorgeHarbor@users.noreply.github.com>
2024-02-17 20:47:11 +0100

    posts controller (#6)
    * Add PostsGetAll
    
    * Add PostsGet
    
    * Add PostsCreate
    
    * Add PostsUpdate
    
    * Add PostsDelete

---
+++ controllers/postsController.go
@@ -0,0 +1,115 @@
+package controllers
+
+import (
+	"github.com/GeorgeHarbor/GoApi/initializers"
+	"github.com/GeorgeHarbor/GoApi/models"
+	"github.com/gin-gonic/gin"
+)
+
+
+func PostsCreate (c *gin.Context) {
+  // Get data off req body
+  var body struct{
+    Title string
+    Body string
+  }
+
+  c.Bind(&body)
+  
+  // Create a post
+  post := models.Post{Title: body.Title, Body: body.Body}
+
+  result := initializers.DB.Create(&post)
+
+  if result.Error != nil{
+    c.Status(400)
+    return 
+  }
+
+  
+  // Return it
+  c.JSON(200, gin.H{
+    "post": post,
+    })
+	}
+
+func PostsGetAll (c *gin.Context){
+  // Get the posts
+  var posts []models.Post
+  result := initializers.DB.Find(&posts)
+
+  // Respond
+  if result.Error != nil {
+    c.Status(404)
+    return
+  }
+  
+  c.JSON(200, gin.H{
+    "posts": posts,
+  })
+}
+
+func PostsGet (c *gin.Context){
+  // Get id from URL
+  id := c.Param("id")
+
+  // Get the post
+
+  var post models.Post
+  result := initializers.DB.First(&post, id)
+
+  // Respond
+  if result.Error != nil {
+    c.Status(404)
+    return
+  }
+  
+  c.JSON(200, gin.H{
+    "post": post,
+  })
+}
+
+func PostsUpdate (c *gin.Context){
+  // Get values from request body
+  id := c.Param("id")
+  
+  var body struct {
+    Title string
+    Body string
+  }
+  
+  c.Bind(&body)
+  
+  // Get the post
+  var post models.Post
+  initializers.DB.First(&post, id)
+
+  // Update the post 
+  result := initializers.DB.Model(&post).Updates(models.Post{Title: body.Title, Body: body.Body,})
+
+  // Return the new post
+  if result.Error != nil {
+    c.Status(400)
+    return
+  }
+
+  c.JSON(200, gin.H{
+    "post": post,
+  })
+}
+
+func PostsDelete (c *gin.Context) {
+  // Get id param
+  id := c.Param("id")
+  
+  // Delete the post
+  var post models.Post
+  result := initializers.DB.Delete(&post, id)
+  
+  // Respond
+  if result.Error != nil{
+    c.Status(404)
+    return
+  }
+ c.JSON(200, "Succefull") 
+}
--- main.go
+++ main.go
@@ -1,4 +1,16 @@
 package main
 
+import (
+	"github.com/GeorgeHarbor/GoApi/controllers"
+	"github.com/gin-gonic/gin"
+)
+
 func main()  {
+ 	r := gin.Default()
+	r.POST("/posts", controllers.PostsCreate)
+  r.PUT("/posts/:id", controllers.PostsUpdate)
+	r.GET("/posts", controllers.PostsGetAll)
+  r.GET("/posts/:id", controllers.PostsGet)
+  r.DELETE("/posts/:id", controllers.PostsDelete)
+	r.Run() 
 }
